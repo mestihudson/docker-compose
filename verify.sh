cbcf7ef629afa20cc5c5ecb9b6f061305310ff11
mestihudson <mestihudson@gmail.com>
2019-05-06 00:23:02 -0300

    ...........

--- verify.sh
+++ verify.sh
@@ -3,9 +3,10 @@
 docker-compose up -d
 
 while true; do
-  test -e cucumber.ok && break || sleep 10 && echo 'tentando...'
-  docker-compose logs frontend acceptance
+  test -e cucumber.ok && break || sleep 10
 done
+docker-compose logs frontend
+docker-compose logs acceptance
 docker-compose exec frontend /bin/sh -c 'yarn build'
 
 exit 0
